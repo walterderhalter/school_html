2a3338f10dbb897cc4788b9b2a156a711ca09e0d
BlingChing <75093890+BlingChing@users.noreply.github.com>
2022-07-21 14:06:35 +0200

    Update

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <div class="header" id="sickystickyHeader">
     <ul class="navbar">
-      <li class="navbarItem"><a href="indexx.php">Start</a></li>
+      <li class="navbarItem"><a href="index.php">Start</a></li>
       <li class="navbarItem"><a href="test.php">Quiz</a></li>
-      <li class="navbarItem"><a href="test.php">Top 10</a></li>
+      <li class="navbarItem"><a href="Top10.php">Top 10</a></li>
       <li class="navbarItem"><a href="#kontakt">Kontakt</a></li>
     </ul>
   </div>
