22a05132e222315960c4c5ce97de98a56ee3ec40
BlingChing <75093890+BlingChing@users.noreply.github.com>
2022-07-25 12:57:34 +0200

    Update Apfel.php

--- Apfel.php
+++ Apfel.php
@@ -2,226 +2,306 @@
 <?php include('nav.php'); ?>
 
 <main id="Apfel">
-<h1> Platz 1: Der Apfel </h1>
+  <h1>Platz 1: Der Apfel</h1>
 
-<div class="Einleitung">
+  <div class="Einleitung">
+    In dem Text werden alle wissenswerte Dinge über Äpfel präsentiert. <br />
+    Wir erklären dir, weshalb
+    <a href="Apfel.php" target="_self"> der Apfel</a> beste Frucht ist. <br />
+  </div>
 
-    In dem Text werden alle wissenswerte Dinge über Äpfel präsentiert. <br>
-    Wir erklären dir, weshalb <a href="Apfel.php" target="_self"> der Apfel</a> beste Frucht ist. <br>
+  <div class="Geschmak">
+    <h2>Der Geschmack</h2>
+    Der Geschmack des Apfels ist vielfältig. <br />
+    Den es kommt ganz auf die Herkunft und Beschaffenheit von Äpfeln an.<br />
+    Mal sind die sauer, mal zuckersüß.<br />
+    Hier hast du mal eine kleine Liste, welche Sorten von Äpfel die belietesten
+    sind und vorallem, wie diese schmecken :P
+    <hr />
+  </div>
 
-</div>
+  <table>
+    <tbody>
+      <tr>
+        <td>
+          <img
+            src="https://www.ndr.de/ratgeber/kochen/warenkunde/elstar101_v-contentxl.jpg"
+            alt="Elstar"
+            width="330"
+            height="240"
+          />
+        </td>
+        <td>
+          <img
+            src="https://www.ndr.de/ratgeber/kochen/warenkunde/jonagold101_v-contentxl.jpg"
+            alt="Jonagold"
+            width="330"
+            height="240"
+          />
+        </td>
+        <td>
+          <img
+            src="https://quiz-media.stroeermediabrands.de/fd/30/8e/12c08415ec55d79b187a61df59_YyAzMDAweDE2ODgrMCszMzQCcmUgNDgwIDI3MANmMDEwMWNkYjcxNQ==.jpg"
+            alt="vergifterter Apfel"
+            width="330"
+            height="240"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Elstar ist eine der beliebtesten Apfelsorten im <br />
+          Norden. Sein saftiges, weiß-gelbliches Fruchtfleisch <br />
+          hat einen intensiven, fein säuerlichen Geschmack. <br />
+          Bei guter Lagerung schmeckt er bis in den Mai.<br />
+        </td>
+        <td>
+          Die meist recht großen Früchte des Jonagold sind <br />
+          sonnenseitig leuchtend rot und glänzend. <br />
+          Sie schmecken süß bis fein säuerlich.<br />
+          Er gehört zu den häufigsten Sorten im Norden.
+        </td>
+        <td>
+          Der giftige Apfel von Schneewittchen ist nicht <br />
+          zu empfehlen
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <hr />
 
-<div class="Geschmak">
-<h2> Der Geschmack </h2>
-Der Geschmack des Apfels ist vielfältig. <br>
-Den es kommt ganz auf die Herkunft und Beschaffenheit von Äpfeln an.<br>
-Mal sind die sauer, mal zuckersüß.<br>
-Hier hast du mal eine kleine Liste, welche Sorten von Äpfel die belietesten sind und vorallem, wie diese schmecken :P <hr>
-</div>
+  <div class="Geruch">
+    <h2>Geruch</h2>
+    Der Geruch ist eine aussagekräftige Aussage dafür, ob die Frucht noch gut
+    ist oder nicht. <br />
+    Den reife Äpfel duften. Äpfel verströmen über ihre Schale ein Aroma, das von
+    zitronig-leichten Düften bis hin zu erdigen Noten reicht.<br />
+    Es gibt auch eine gute Faustregel:<br />
+    <blockquote
+      cite="https://de.vapiano.com/de/blog/fuehlen-klopfen-riechen-so-erkennt-ihr-reife-fruechte/#:~:text=Vor%20allem%20Äpfel%20verströmen%20über,sie%20fast%20schon%20überreif%20sind."
+    >
+      <p>
+        &bdquo;Nehmt ein Stück Obst in die Hand, und schnuppert daran! Je besser
+        der Duft, umso reifer die Frucht.&ldquo;
+      </p>
+    </blockquote>
+  </div>
+  <hr />
 
-<table>
-<tbody>
-<tr>
-<td><img src="https://www.ndr.de/ratgeber/kochen/warenkunde/elstar101_v-contentxl.jpg" alt="Elstar" width="330" height="240"/></td>
-<td><img src="https://www.ndr.de/ratgeber/kochen/warenkunde/jonagold101_v-contentxl.jpg" alt="Jonagold" width="330" height="240"/></td>
-<td><img src="https://quiz-media.stroeermediabrands.de/fd/30/8e/12c08415ec55d79b187a61df59_YyAzMDAweDE2ODgrMCszMzQCcmUgNDgwIDI3MANmMDEwMWNkYjcxNQ==.jpg" alt="vergifterter Apfel" width="330" height="240"/></td>
-</tr>
-<tr>
-<td> Elstar ist eine der beliebtesten Apfelsorten im <br>
-    Norden. 
-    Sein saftiges, weiß-gelbliches Fruchtfleisch <br>
-    hat 
-    einen intensiven, fein säuerlichen Geschmack. <br>
-    Bei guter Lagerung schmeckt er bis in den Mai.<br></td>
-<td> Die meist recht großen Früchte des Jonagold sind <br>
-    sonnenseitig leuchtend rot und glänzend. <br>
-    Sie schmecken süß bis fein säuerlich.<br>
-     Er gehört zu den häufigsten Sorten im Norden. </td>
-<td>Der giftige Apfel von Schneewittchen ist nicht <br>
-     zu empfehlen </td>
-</tr>
-</tbody>
-</table> <hr>
-
-<div class="Geruch">
-<h2> Geruch </h2>
-Der Geruch ist eine aussagekräftige Aussage dafür, ob die Frucht noch gut ist oder nicht. <br>
-Den reife Äpfel duften. Äpfel verströmen über ihre Schale ein Aroma, das von zitronig-leichten Düften bis hin zu erdigen Noten reicht.<br>
-Es gibt auch eine gute Faustregel:<br>
-<blockquote cite="https://de.vapiano.com/de/blog/fuehlen-klopfen-riechen-so-erkennt-ihr-reife-fruechte/#:~:text=Vor%20allem%20Äpfel%20verströmen%20über,sie%20fast%20schon%20überreif%20sind.">
-  <p>&bdquo;Nehmt ein Stück Obst in die Hand, und schnuppert daran! 
-   Je besser der Duft, umso reifer die Frucht.&ldquo;</p>
-</blockquote>
-</div>
-<hr>
-
-<div class ="Gesundheit">
-<h2> Gesundheit</h2>
-An dieser Stelle möchten wir auf ein Video verweisen. <br>
-Hier geht es zum Video: 
-<p>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/iWrfsSzboLY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</p>
-</div>
-
-<hr>
-
-<div class ="Beliebtheit">
-    <h3> Beliebtheit </h3>
+  <div class="Gesundheit">
+    <h2>Gesundheit</h2>
+    An dieser Stelle möchten wir auf ein Video verweisen. <br />
+    Hier geht es zum Video:
     <p>
-    Der Apfel ist die beliebteste Frucht ganz Deutschland. <br>
-    Damit du auch vor Augen hast, wie beliebt die Frucht der Deutschen ist, <br>
-    zeigen wir dir an 5 Fakten: 
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/iWrfsSzboLY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </p>
-</div>
+  </div>
+
+  <hr />
+
+  <div class="Beliebtheit">
+    <h3>Beliebtheit</h3>
+    <p>
+      Der Apfel ist die beliebteste Frucht ganz Deutschland. <br />
+      Damit du auch vor Augen hast, wie beliebt die Frucht der Deutschen ist,
+      <br />
+      zeigen wir dir an 5 Fakten:
+    </p>
+  </div>
 </main>
 
-    <artical class="fivefact">
-    <h3> 5 Fakten über Äpfel </h3>
+<artical class="fivefact">
+  <h3>5 Fakten über Äpfel</h3>
 
-    <section class="fact">
+  <section class="fact">
     <h4>Fakt 1</h4>
     <p>
-    Deutsche lieben Äpfel: <br>
-    Fast <enum>25 Kilogramm</enum> verputzt jeder von uns im Jahr, <br>
-    etwa im Kuchen, als Saft oder auch als pure Frucht.<br>
-    Das sind insgesamt rund zwei Millionen Tonnen! </p><br>
-    </section>
+      Deutsche lieben Äpfel: <br />
+      Fast <enum>25 Kilogramm</enum> verputzt jeder von uns im Jahr, <br />
+      etwa im Kuchen, als Saft oder auch als pure Frucht.<br />
+      Das sind insgesamt rund zwei Millionen Tonnen!
+    </p>
+    <br />
+  </section>
 
-    <section class="fact">
+  <section class="fact">
     <h4>Fakt 2</h4>
-    <p>Deutsche lieben Äpfel: <br>
-    Etwa sieben Jahre muss ein Apfelbaum aus einem Samen wachsen, <br>
-    bis er zum ersten Mal Früchte trägt. <br>
-    Dafür kann er bis zu 100 Jahre alt werden. </p><br>
-    </section>
+    <p>
+      Deutsche lieben Äpfel: <br />
+      Etwa sieben Jahre muss ein Apfelbaum aus einem Samen wachsen, <br />
+      bis er zum ersten Mal Früchte trägt. <br />
+      Dafür kann er bis zu 100 Jahre alt werden.
+    </p>
+    <br />
+  </section>
 
-    <section class="fact">
+  <section class="fact">
     <h4>Fakt 3</h4>
-   <p> Kein Wunder, dass er so saftig ist: <br>
-    Ein Apfel besteht zu 85 Prozent aus Wasser. <br>
-    Der Rest sind Kohlenhydrate, Fett, Eiweiß, Mineralstoffe und Vitamine. </p> <br>
-    </section>
+    <p>
+      Kein Wunder, dass er so saftig ist: <br />
+      Ein Apfel besteht zu 85 Prozent aus Wasser. <br />
+      Der Rest sind Kohlenhydrate, Fett, Eiweiß, Mineralstoffe und Vitamine.
+    </p>
+    <br />
+  </section>
 
-    <section class="fact">
+  <section class="fact">
     <h4>Fakt 4</h4>
-    <p>Der größte Apfel der Welt wurde laut Guinnessbuch <br>
-    der Rekorde 2005 in Japan von Chisato Iwasaki geerntet: <br>
-    Das Prachtstück wog 1,849 Kilogramm. </p> <br>
-    </section>
+    <p>
+      Der größte Apfel der Welt wurde laut Guinnessbuch <br />
+      der Rekorde 2005 in Japan von Chisato Iwasaki geerntet: <br />
+      Das Prachtstück wog 1,849 Kilogramm.
+    </p>
+    <br />
+  </section>
 
-    <section class="fact">
+  <section class="fact">
     <h4>Fakt 5</h4>
-    <p>Etwa 30.000 Apfelsorten gibt es weltweit, davon 2000 in Deutschland. <br>
-    In den Supermärkten liegen aber meist nur Sorten wie <br>
-    Elstar, Jonagold, Golden Delicious oder Braeburn</p><br>
-    </section>
-
-    <p>Quelle: <a href="https://www.geo.de/geolino/natur-und-umwelt/14740-rtkl-gib-mir-fuenf-fuenfmal-staunen-ueber-aepfel#:~:text=Etwa%20sieben%20Jahre%20muss%20ein,%2C%20Eiwei%C3%9F%2C%20Mineralstoffe%20und%20Vitamine." 
-    target="_self"> Link</a></p>
-    </artical>
-
-<hr>
-
-<div class ="Inhaltsstoffe">
-    <h3> Inhaltsstoffe </h3>
-
-    <p> 
-        Hier noch eine Übersicht zu den Inhaltsstoffe von einem Apfel:
+    <p>
+      Etwa 30.000 Apfelsorten gibt es weltweit, davon 2000 in Deutschland.
+      <br />
+      In den Supermärkten liegen aber meist nur Sorten wie <br />
+      Elstar, Jonagold, Golden Delicious oder Braeburn
     </p>
+    <br />
+  </section>
 
-<table class="table"> 
-    <caption>Übersichtstabelle</caption>
-<thead>
-    <tr> 
-    <th> Apfel Inhaltsstoffe</th> 
-    <th> <p>je 100 <abbr lang="de" title="Gramm">g</abbr> verzehr-<br>barer Anteil, ungeschält, roh</p></th> 
-    <th> Apfel Inhaltsstoffe</th> 
-    <th> je 100 <abbr lang="de" title="Gramm">g</abbr> verzehrbarer <br>Anteil, ungeschält und roh</th> 
-   </tr> 
-</thead>
-  <tbody> 
-   <tr> 
-    <td> Energie (<abbr lang="de" title="Kilokalorien">kcal</abbr>)</td> 
-    <td> 54</td> 
-    <td> Vitamin A (<abbr lang="de" title="Mikrogramm">µg</abbr>)</td> 
-    <td> 6</td> 
-   </tr> 
-   <tr> 
-    <td> Fett (<abbr lang="de" title="Gramm">g</abbr>)</td> 
-    <td> 0,6</td> 
-    <td> Vitamin B1 (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,04</td> 
-   </tr> 
-   <tr> 
-    <td> Protein (<abbr lang="de" title="Gramm">g</abbr>)</td> 
-    <td> 0,3</td> 
-    <td> Vitamin B2 (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,03</td> 
-   </tr> 
-   <tr> 
-    <td> Kohlenhydrate (<abbr lang="de" title="Gramm">g</abbr>)</td> 
-    <td> 11,4</td> 
-    <td> Niacin (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,3</td> 
-   </tr> 
-   <tr> 
-    <td> Ballaststoffe (<abbr lang="de" title="Gramm">g</abbr>)</td> 
-    <td> 2,0</td> 
-    <td> Vitamin B6 (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,1</td> 
-   </tr> 
-   <tr> 
-    <td> Kalium (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 122</td> 
-    <td> Folsäure (<abbr lang="de" title="Mikrogramm">µg</abbr>)</td> 
-    <td> 7,5</td> 
-   </tr> 
-   <tr> 
-    <td> Kalzium (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 7 </td> 
-    <td> Vitamin C (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 12</td> 
-   </tr> 
-   <tr> 
-    <td> Magnesium (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 6</td> 
-    <td> Vitamin E (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,5</td> 
-   </tr> 
-   <tr> 
-    <td> Eisen (<abbr lang="de" title="Milligramm">mg</abbr>)</td> 
-    <td> 0,5</td> 
-    <td> </td> 
-    <td> </td> 
-   </tr> 
-  </tbody> 
-  <tfoot>
+  <p>
+    Quelle:
+    <a
+      href="https://www.geo.de/geolino/natur-und-umwelt/14740-rtkl-gib-mir-fuenf-fuenfmal-staunen-ueber-aepfel#:~:text=Etwa%20sieben%20Jahre%20muss%20ein,%2C%20Eiwei%C3%9F%2C%20Mineralstoffe%20und%20Vitamine."
+      target="_self"
+    >
+      Link</a
+    >
+  </p>
+</artical>
 
-    <tr> 
-    <th> Apfel Inhaltsstoffe</th> 
-    <th> <p>je 100 <abbr lang="de" title="Gramm">g</abbr> verzehr-<br>barer Anteil, ungeschält, roh</p></th> 
-    <th> Apfel Inhaltsstoffe</th> 
-    <th> je 100 <abbr lang="de" title="Gramm">g</abbr> verzehrbarer <br>Anteil, ungeschält und roh</th> 
-   </tr> 
+<hr />
 
-</tfoot>
+<div class="Inhaltsstoffe">
+  <h3>Inhaltsstoffe</h3>
 
- </table>
- <p>Quelle: <a href="https://www.gesundheit.gv.at/leben/ernaehrung/saisonkalender/alle/apfel.html" 
-    target="_self"> Link</a></p>
-    </div>
-    <hr>
+  <p>Hier noch eine Übersicht zu den Inhaltsstoffe von einem Apfel:</p>
 
+  <table class="table">
+    <caption>
+      Übersichtstabelle
+    </caption>
+    <thead>
+      <tr>
+        <th>Apfel Inhaltsstoffe</th>
+        <th>
+          <p>
+            je 100 <abbr lang="de" title="Gramm">g</abbr> verzehr-<br />barer
+            Anteil, ungeschält, roh
+          </p>
+        </th>
+        <th>Apfel Inhaltsstoffe</th>
+        <th>
+          je 100 <abbr lang="de" title="Gramm">g</abbr> verzehrbarer
+          <br />Anteil, ungeschält und roh
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Energie (<abbr lang="de" title="Kilokalorien">kcal</abbr>)</td>
+        <td>54</td>
+        <td>Vitamin A (<abbr lang="de" title="Mikrogramm">µg</abbr>)</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>Fett (<abbr lang="de" title="Gramm">g</abbr>)</td>
+        <td>0,6</td>
+        <td>Vitamin B1 (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,04</td>
+      </tr>
+      <tr>
+        <td>Protein (<abbr lang="de" title="Gramm">g</abbr>)</td>
+        <td>0,3</td>
+        <td>Vitamin B2 (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,03</td>
+      </tr>
+      <tr>
+        <td>Kohlenhydrate (<abbr lang="de" title="Gramm">g</abbr>)</td>
+        <td>11,4</td>
+        <td>Niacin (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,3</td>
+      </tr>
+      <tr>
+        <td>Ballaststoffe (<abbr lang="de" title="Gramm">g</abbr>)</td>
+        <td>2,0</td>
+        <td>Vitamin B6 (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,1</td>
+      </tr>
+      <tr>
+        <td>Kalium (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>122</td>
+        <td>Folsäure (<abbr lang="de" title="Mikrogramm">µg</abbr>)</td>
+        <td>7,5</td>
+      </tr>
+      <tr>
+        <td>Kalzium (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>7</td>
+        <td>Vitamin C (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>12</td>
+      </tr>
+      <tr>
+        <td>Magnesium (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>6</td>
+        <td>Vitamin E (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,5</td>
+      </tr>
+      <tr>
+        <td>Eisen (<abbr lang="de" title="Milligramm">mg</abbr>)</td>
+        <td>0,5</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th>Apfel Inhaltsstoffe</th>
+        <th>
+          <p>
+            je 100 <abbr lang="de" title="Gramm">g</abbr> verzehr-<br />barer
+            Anteil, ungeschält, roh
+          </p>
+        </th>
+        <th>Apfel Inhaltsstoffe</th>
+        <th>
+          je 100 <abbr lang="de" title="Gramm">g</abbr> verzehrbarer
+          <br />Anteil, ungeschält und roh
+        </th>
+      </tr>
+    </tfoot>
+  </table>
+  <p>
+    Quelle:
+    <a
+      href="https://www.gesundheit.gv.at/leben/ernaehrung/saisonkalender/alle/apfel.html"
+      target="_self"
+    >
+      Link</a
+    >
+  </p>
+</div>
+<hr />
 
-    <p> 
-        Wir hoffen, dass der Artikel dir gefallen hat. Hier geht es weiter zu Platz 2
-    </p>
-    <div class="Rechtsbündig">
-            <a href="Banane.php" 
-            target="_self"> Platz 2: Die Banane</a>
-    </div>
-
-
+<p>
+  Wir hoffen, dass der Artikel dir gefallen hat. Hier geht es weiter zu Platz 2
+</p>
+<div class="Rechtsbündig">
+  <a href="Banane.php" target="_self"> Platz 2: Die Banane</a>
+</div>
 
 <?php include('footer.php'); ?>
