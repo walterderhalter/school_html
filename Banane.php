7d96adbbbb8e30cb3dd09f27952748c0813b672e
BlingChing <75093890+BlingChing@users.noreply.github.com>
2022-07-25 13:01:37 +0200

    Update Banane.php

--- Banane.php
+++ Banane.php
@@ -2,184 +2,228 @@
 <?php include('nav.php'); ?>
 
 <aside class="Test">
-    <a href="https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg">
-        <img src="https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg" width="200" height="310"
-      alt="Banane Minion">
-    </a>
-    <br>
-    <a href="https://i.otto.de/i/otto/5ac1c5bb-bcd0-5f76-a4ba-28499fa0d58c?h=520&w=551&sm=clamp">
-        <img src="https://i.otto.de/i/otto/5ac1c5bb-bcd0-5f76-a4ba-28499fa0d58c?h=520&w=551&sm=clamp" width="200" height="310"
-      alt="Bananen Kostum">
-    </a>
-    <br>
-    <a href="https://www.wwf.ch/sites/default/files/styles/key_visual_large_16_10/public/2021-03/Bananen.jpg?h=733e6470&itok=EUFofKU1">
-        <img src="https://www.wwf.ch/sites/default/files/styles/key_visual_large_16_10/public/2021-03/Bananen.jpg?h=733e6470&itok=EUFofKU1" width="200" height="200"
-      alt="Bananen Banana">
-    </a>
-    <a href="https://previews.123rf.com/images/shadowstudio/shadowstudio1511/shadowstudio151100002/48725846-gl%C3%BCckliche-nette-affe-die-banane-isst.jpg">
-        <img src="https://previews.123rf.com/images/shadowstudio/shadowstudio1511/shadowstudio151100002/48725846-gl%C3%BCckliche-nette-affe-die-banane-isst.jpg" width="200" height="300"
-      alt="Bananen Affe">
-    </a>
-    <a href="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg">
-        <img src="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg" width="200" height="220"
-      alt="Bananen Love">
-    </a>
-
+  <a
+    href="https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg"
+  >
+    <img
+      src="https://m.media-amazon.com/images/M/MV5BMzMwYjc1N2MtY2U2Ny00MTc3LTk1YWQtYzE3NmM5NWQ2YzkyXkEyXkFqcGdeQXVyMzAzODY0NzE@._V1_.jpg"
+      width="200"
+      height="310"
+      alt="Banane Minion"
+    />
+  </a>
+  <br>
+  <a
+    href="https://i.otto.de/i/otto/5ac1c5bb-bcd0-5f76-a4ba-28499fa0d58c?h=520&w=551&sm=clamp"
+  >
+    <img
+      src="https://i.otto.de/i/otto/5ac1c5bb-bcd0-5f76-a4ba-28499fa0d58c?h=520&w=551&sm=clamp"
+      width="200"
+      height="310"
+      alt="Bananen Kostum"
+    />
+  </a>
+  <br>
+  <a
+    href="https://www.wwf.ch/sites/default/files/styles/key_visual_large_16_10/public/2021-03/Bananen.jpg?h=733e6470&itok=EUFofKU1"
+  >
+    <img
+      src="https://www.wwf.ch/sites/default/files/styles/key_visual_large_16_10/public/2021-03/Bananen.jpg?h=733e6470&itok=EUFofKU1"
+      width="200"
+      height="200"
+      alt="Bananen Banana"
+    />
+  </a>
+  <a
+    href="https://previews.123rf.com/images/shadowstudio/shadowstudio1511/shadowstudio151100002/48725846-gl%C3%BCckliche-nette-affe-die-banane-isst.jpg"
+  >
+    <img
+      src="https://previews.123rf.com/images/shadowstudio/shadowstudio1511/shadowstudio151100002/48725846-gl%C3%BCckliche-nette-affe-die-banane-isst.jpg"
+      width="200"
+      height="300"
+      alt="Bananen Affe"
+    />
+  </a>
+  <a
+    href="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
+  >
+    <img
+      src="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
+      width="200"
+      height="220"
+      alt="Bananen Love"
+    />
+  </a>
 </aside>
 
-
-
 <main id="Banana">
-<h1> Platz 2: Die Banane </h1>
+  <h1>Platz 2: Die Banane</h1>
 
-<div class="Einleitung">
+  <div class="Einleitung">
+    In dem Text werden alle wissenswerte Dinge über die Bananen präsentiert.
+    <br>
+    Wir erklären dir, weshalb
+    <a href="Banane.php" target="_self"> die Banane</a> die zweitbeste Frucht
+    ist. <br>
+  </div>
 
-    In dem Text werden alle wissenswerte Dinge über die Bananen präsentiert. <br>
-    Wir erklären dir, weshalb <a href="Banane.php" target="_self"> die Banane</a> die zweitbeste Frucht ist. <br>
-
-</div>
-
-<div class="Geschmak">
-    <h2> Geschmak </h2>
+  <div class="Geschmak">
+    <h2>Geschmak</h2>
 
     <p>
-        Reife Bananen haben einen angenehmen süßlichen Geschmack. <br>
-        Wenn sie noch leicht grün sind, können sie eine leicht säuerliche Note beinhalten. <br>
-        Außerdem ist das Fruchtfleisch bei diesem Reifegrad noch fester. 
+      Reife Bananen haben einen angenehmen süßlichen Geschmack. <br>
+      Wenn sie noch leicht grün sind, können sie eine leicht säuerliche Note
+      beinhalten. <br>
+      Außerdem ist das Fruchtfleisch bei diesem Reifegrad noch fester.
     </p>
 
-    Quelle: 
-        <a href="https://www.gutekueche.at/banane-artikel-1119#:~:text=gerade%20wachsende%20Bananensorten.-,Geschmack,bei%20diesem%20Reifegrad%20noch%20fester.">
-            Link 
-        </a> 
+    Quelle:
+    <a
+      href="https://www.gutekueche.at/banane-artikel-1119#:~:text=gerade%20wachsende%20Bananensorten.-,Geschmack,bei%20diesem%20Reifegrad%20noch%20fester."
+    >
+      Link
+    </a>
     <br>
 
-<p>
-Selbst die Minions lieben Bananen <br>
-Es gibt sogar ein Banana-Song (ab 0:50 wird es richtig nervig).
-</p>
-
-<audio controls>
-<source src="assets/banana song - minions.mp3">
-</audio>
-
-</div>
-
-<hr>
-
-<div class="Geruch">
-
-
-<div class="FaktBanana">
-    <strong>Zusatzfakt: </strong> <br>
- <em>Männliche Mäuse hassen den Geruch von Bananen.</em>
-</div>
-<h2> Geruch </h2>
-<p>
-Bananen besitzen einen ganz leicht angenehmen Geruch. Treten einige Bananen <br>
-ins Klimakterium (turner),<br>
- wird der von der Ware ausgehende Geruch merklich intensiver.
-</p>
-
-<p>
-Quelle Geruch: <a href="https://www.tis-gdv.de/tis/ware/obst/banane/banane-htm/#:~:text=Bananen%20besitzen%20einen%20ganz%20leicht,Ware%20ausgehende%20Geruch%20merklich%20intensiver.">
-    Link </a> <br>
-Quelle Fakt: <a href="https://www.stern.de/panorama/wissen/natur/warum-hassen-maennliche-maeuse-den-geruch-von-bananenschalen--31913812.html">
-    Link </a> 
-</p>
-</div>
-
-<hr>
-
-
-<div class ="Beliebtheit und Gesundheit">
-    <h2> Beliebtheit und Gesundheit</h2>
-    <img src="https://imagemarker.com/editor/storage/app/images/4N5QWhMJxwQ08fzX1uGOeV7Cgfx9nNizgy944QiQ.jpeg" alt="Gesunde Banana" width="500" height="500"/>
-    <img src="https://rohtopia.com/wp-content/uploads/2013/01/10630618_901183409900251_3408876877771360536_o.jpg" alt="Gesunde Banana" width="500" height="500"/>
-</div>
-
-<hr>
-
-<div class ="Inhaltsstoffe">
-    <h3> Inhaltsstoffe </h3>
-
-    <p> 
-        Hier noch eine Übersicht zu den Inhaltsstoffe von einer Banane:
+    <p>
+      Selbst die Minions lieben Bananen <br>
+      Es gibt sogar ein Banana-Song (ab 0:50 wird es richtig nervig).
     </p>
 
-<table class="table"> 
-<caption>Übersichtstabelle</caption>
-<thead>
-    <tr class="dunkelgelb">
-    <th> Banane Inhaltsstoffe</th> 
-    <th> <p>Dosierung pro 100  <abbr lang="de" title="Gramm">g</abbr></p></th> 
-</thead>
+    <audio controls>
+      <source src="assets/banana song - minions.mp3" />
+    </audio>
+  </div>
 
-  <tbody> 
-  <tr class="gelb">
-    <td> Kohlenhydrate (<abbr title="Kilokalorien">kcal</abbr>)</td> 
-    <td> 90</td> 
-   </tr> 
+  <hr>
 
-   <tr class="blassgelb">
-    <td> Vitamin C </td> 
-    <td> 11 <abbr title="Milligramm">mg</abbr></td> 
-   </tr> 
-
-    <tr class="gelb">
-    <td> Folsäure </td> 
-    <td> 2,0 <abbr title="Microgramm">µg</abbr></td> 
-   </tr> 
-
-   <tr class="blassgelb"> 
-    <td> Kalium </td> 
-    <td> 380 <abbr title="Milligramm">mg</abbr></td>
-   </tr> 
-
-   <tr class="gelb">
-    <td> Magnesium </td> 
-    <td> 30 <abbr title="Milligramm">mg</abbr></td> 
-   </tr> 
-
-   <tr class="blassgelb">
-    <td> Beta-Carotin </td> 
-    <td> 30 <abbr title="Microgramm">µg</abbr></td> 
-   </tr> 
-
-   <tr class="gelb">
-    <td> Phosphor </td> 
-    <td> 22 <abbr title="Milligramm">mg</abbr></td> 
-   </tr> 
-  </tbody> 
-
-
- </table>
-
-    
- <p>    Quelle: <a href="https://www.gesundheitswissen.de/ernaehrung/gesunde-lebensmittel/obst-und-gemuese/alles-banane-oder-was/" 
-    target="_self"> Link</a>
-    </p>
+  <div class="Geruch">
+    <div class="FaktBanana">
+      <strong>Zusatzfakt: </strong> <br />
+      <em>Männliche Mäuse hassen den Geruch von Bananen.</em>
     </div>
-    <hr>
+    <h2>Geruch</h2>
+    <p>
+      Bananen besitzen einen ganz leicht angenehmen Geruch. Treten einige
+      Bananen <br />
+      ins Klimakterium (turner),<br />
+      wird der von der Ware ausgehende Geruch merklich intensiver.
+    </p>
 
-<p> 
-  Wir hoffen, dass der Artikel dir gefallen hat. Hier geht es weiter:
-</p>
-    
-        <div class="Linksbündig">
-            <a href="Apfel.php" 
-            target="_self"> Platz 1: Die Apfel</a>
-        </div>
+    <p>
+      Quelle Geruch:
+      <a
+        href="https://www.tis-gdv.de/tis/ware/obst/banane/banane-htm/#:~:text=Bananen%20besitzen%20einen%20ganz%20leicht,Ware%20ausgehende%20Geruch%20merklich%20intensiver."
+      >
+        Link
+      </a>
+      <br />
+      Quelle Fakt:
+      <a
+        href="https://www.stern.de/panorama/wissen/natur/warum-hassen-maennliche-maeuse-den-geruch-von-bananenschalen--31913812.html"
+      >
+        Link
+      </a>
+    </p>
+  </div>
 
-        <div class="Rechtsbündig">
-            <a href="Pfirsich.php" 
-            target="_self"> Platz 3: Der Pfirsich</a>
-        </div>
-    
+  <hr>
 
+  <div class="Beliebtheit und Gesundheit">
+    <h2>Beliebtheit und Gesundheit</h2>
+    <img
+      src="https://imagemarker.com/editor/storage/app/images/4N5QWhMJxwQ08fzX1uGOeV7Cgfx9nNizgy944QiQ.jpeg"
+      alt="Gesunde Banana"
+      width="500"
+      height="500"
+    />
+    <img
+      src="https://rohtopia.com/wp-content/uploads/2013/01/10630618_901183409900251_3408876877771360536_o.jpg"
+      alt="Gesunde Banana"
+      width="500"
+      height="500"
+    />
+  </div>
+
+  <hr>
+
+  <div class="Inhaltsstoffe">
+    <h3>Inhaltsstoffe</h3>
+
+    <p>Hier noch eine Übersicht zu den Inhaltsstoffe von einer Banane:</p>
+
+    <table class="table">
+      <caption>
+        Übersichtstabelle
+      </caption>
+      <thead>
+        <tr class="dunkelgelb">
+          <th>Banane Inhaltsstoffe</th>
+          <th>
+            <p>Dosierung pro 100 <abbr lang="de" title="Gramm">g</abbr></p>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr class="gelb">
+          <td>Kohlenhydrate (<abbr title="Kilokalorien">kcal</abbr>)</td>
+          <td>90</td>
+        </tr>
+
+        <tr class="blassgelb">
+          <td>Vitamin C</td>
+          <td>11 <abbr title="Milligramm">mg</abbr></td>
+        </tr>
+
+        <tr class="gelb">
+          <td>Folsäure</td>
+          <td>2,0 <abbr title="Microgramm">µg</abbr></td>
+        </tr>
+
+        <tr class="blassgelb">
+          <td>Kalium</td>
+          <td>380 <abbr title="Milligramm">mg</abbr></td>
+        </tr>
+
+        <tr class="gelb">
+          <td>Magnesium</td>
+          <td>30 <abbr title="Milligramm">mg</abbr></td>
+        </tr>
+
+        <tr class="blassgelb">
+          <td>Beta-Carotin</td>
+          <td>30 <abbr title="Microgramm">µg</abbr></td>
+        </tr>
+
+        <tr class="gelb">
+          <td>Phosphor</td>
+          <td>22 <abbr title="Milligramm">mg</abbr></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>
+      Quelle:
+      <a
+        href="https://www.gesundheitswissen.de/ernaehrung/gesunde-lebensmittel/obst-und-gemuese/alles-banane-oder-was/"
+        target="_self"
+      >
+        Link</a
+      >
+    </p>
+  </div>
+  <hr>
+
+  <p>Wir hoffen, dass der Artikel dir gefallen hat. Hier geht es weiter:</p>
+
+  <div class="Linksbündig">
+    <a href="Apfel.php" target="_self"> Platz 1: Die Apfel</a>
+  </div>
+
+  <div class="Rechtsbündig">
+    <a href="Pfirsich.php" target="_self"> Platz 3: Der Pfirsich</a>
+  </div>
 </main>
-
-
-
 
 <?php include('footer.php'); ?>
